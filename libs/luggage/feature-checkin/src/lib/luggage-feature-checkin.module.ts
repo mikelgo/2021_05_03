33f67832e38de30f9915f0d89b6673779cf7f3e5
mikelgo <m.gonser@icloud.com>
2021-05-03 11:50:26 +0200

    ddd: add luggage libs: domain, feature-checkin and feature-report-loss

---
+++ libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
+import { CheckinComponent } from './checkin.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    LuggageDomainModule, // Add this:
+    RouterModule.forChild([{ path: '', component: CheckinComponent }]),
+  ],
+  declarations: [CheckinComponent],
+  exports: [CheckinComponent],
+})
+export class LuggageFeatureCheckinModule {}
---
+++ libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
+import { ReportLossComponent } from './report-loss.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    LuggageDomainModule, // Add this:
+    RouterModule.forChild([{ path: '', component: ReportLossComponent }]),
+  ],
+  declarations: [ReportLossComponent],
+  exports: [ReportLossComponent],
+})
+export class LuggageFeatureReportLossModule {}
